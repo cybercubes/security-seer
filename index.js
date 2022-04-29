1be338afdda50897dc18ade8588e88a23d4179af
Kirill <cyberandcubes@gmail.com>
2022-04-29 18:02:35 +0300

    implement dependecy list extraction

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+import { execute } from './function.js';
+
+async function main() {
+  try {
+    let listPromise = await execute("npm list -json");
+    let parsedList = JSON.parse(listPromise);
+
+    console.log(parsedList.dependencies);
+  } catch (error) {
+    console.error(error.toString());
+  }
+}
+
+main();
